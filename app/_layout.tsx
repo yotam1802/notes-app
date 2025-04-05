944d3b7f91c8265b067ca90d27894794e9c65c18
yotam1802 <Yotam1802@gmail.com>
2025-04-05 00:49:41 -0600

    feat: finish app home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#ff8c00",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+        },
+        contentStyle: {
+          paddingHorizontal: 10,
+          paddingTop: 10,
+          backgroundColor: "#fff",
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,66 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import PostItImage from "@/assets/images/post-it.png";
+import { useRouter } from "expo-router";
 
-export default function Index() {
+const HomeScreen = () => {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={PostItImage} style={styles.image} />
+      <Text style={styles.title}>Welcome To Notes App</Text>
+      <Text style={styles.subtitle}>
+        Capture your thoughts anytime, anywhere
+      </Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/notes")}
+      >
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f8f9fa",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default HomeScreen;
